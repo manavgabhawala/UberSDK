90b6911ccd2868fbea2426643e1cf5dd04e7e0ab
manavgabhawala <manav1907@gmail.com>
2015-03-31 04:38:00 -0400

    Initial Commit

---
+++ UberManager.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  UberSDK
+//
+//  Created by Manav Gabhawala on 3/31/15.
+//
+//
+
+import Foundation
+
+
+
+class UberManager
+{
+	
+}
---
+++ UberSDKiOS/UberSDKiOS.h
@@ -0,0 +1,19 @@
+//
+//  UberSDKiOS.h
+//  UberSDKiOS
+//
+//  Created by Manav Gabhawala on 3/31/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UberSDKiOS.
+FOUNDATION_EXPORT double UberSDKiOSVersionNumber;
+
+//! Project version string for UberSDKiOS.
+FOUNDATION_EXPORT const unsigned char UberSDKiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UberSDKiOS/PublicHeader.h>
+
+
